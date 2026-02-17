8ef310a69a613e18e6e5d3c5ee5e0478055db8a9
Anibal Gonzalez Veira <165087800+AnibalGonzalezV@users.noreply.github.com>
2026-02-17 14:10:58 -0300

    Cambio de Insta

--- src/config/site.ts
+++ src/config/site.ts
@@ -11,7 +11,7 @@ export const SITE_CONFIG = {
   
   // Redes Sociales
   links: {
-    instagram: "https://instagram.com/yosoyvidanatural",
+    instagram: "https://instagram.com/yosoyvida.cl",
     facebook: "https://facebook.com/yosoyvida", // Ajusta si tienes
   },
 
